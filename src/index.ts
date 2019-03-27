6a3bddcc819124de1445166c1a19a56eee6dceb4
hideokamoto <hide.okamoto@digitalcube.jp>
2019-03-27 16:18:16 +0900

    search functions

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,67 @@
 import {Command, flags} from '@oclif/command'
+import { Lambda } from 'aws-sdk'
+import chalk from 'chalk'
+
+type ISearchQuery = {
+  Runtime?: string
+}
 
 class LambdaFunctionSearch extends Command {
   static description = 'describe the command here'
+  private NextMarker: string = ''
+  private amount: number = 0
+  private client: Lambda | undefined
 
   static flags = {
-    // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    // runtime
+    runtime: flags.string({char: 'R', description: 'Lambda runtime'}),
+    // region
+    region: flags.string({
+      char: 'r',
+      description: 'region'
+    }),
+  }
+  async listAllFunctions(query: ISearchQuery = {}, functions: Lambda.FunctionList = []): Promise<Lambda.FunctionList> {
+    if (!this.client) {
+      throw new Error('Failed to initilize AWS SDK Class')
+    }
+    const params: Lambda.ListFunctionsRequest = {}
+    if (this.NextMarker) params.Marker = this.NextMarker
+    const { NextMarker, Functions} = await this.client.listFunctions(params).promise()
+    this.NextMarker = NextMarker || ''
+    const targetFunctions = !Functions ? [] : Functions.filter(func => {
+      if (query.Runtime) return func.Runtime === query.Runtime
+      return true
+    })
+    const items = functions.concat(targetFunctions)
+    this.amount += Functions ? Functions.length : 0
+    if (this.NextMarker) {
+      return this.listAllFunctions(query, items)
+    }
+    return items
   }
 
-  static args = [{name: 'file'}]
-
   async run() {
-    const {args, flags} = this.parse(LambdaFunctionSearch)
-
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const {flags} = this.parse(LambdaFunctionSearch)
+    const props: Lambda.Types.ClientConfiguration = {}
+    if (!flags.region) this.log(chalk.yellow('warning') + ': Missing region')
+    if (flags.region) props.region = flags.region
+    this.client = new Lambda(props)
+    const query: ISearchQuery = {}
+    if (flags.runtime) {
+      this.log(`${chalk.green('Search condition')}: Runtime === ${flags.runtime}`)
+      query.Runtime = flags.runtime
+    }
+    try {
+      const result = await this.listAllFunctions(query)
+      this.log(`${chalk.green('Matched Functions')}: ${result.length} / ${this.amount}`)
+      result.forEach(item => {
+        this.log(item.FunctionName)
+      })
+    } catch (e) {
+      this.error(chalk.red(e))
+      this.exit(1)
     }
   }
 }
