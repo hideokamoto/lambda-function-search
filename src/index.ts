f1bb0b096812260d305037f8fa4ad234e0ae17d8
hideokamoto <hide.okamoto@digitalcube.jp>
2019-03-28 15:30:43 +0900

    all region

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,56 @@ import {Command, flags} from '@oclif/command'
 import { Lambda, SharedIniFileCredentials } from 'aws-sdk'
 import chalk from 'chalk'
 
+type IRuntime = "nodejs" | "nodejs4.3" | "nodejs6.10" | "nodejs8.10" | "java8" | "python2.7" | "python3.6" | "python3.7" | "dotnetcore1.0" | "dotnetcore2.0" | "dotnetcore2.1" | "nodejs4.3-edge" | "go1.x" | "ruby2.5" | "provided" | string
 type ISearchQuery = {
-  Runtime?: string,
+  Runtime?: IRuntime,
   name?: string
 }
+interface IQueryBuilder {
+  addRuntime(runtime: IRuntime): IQueryBuilder
+  addSearchQuery(name: string): IQueryBuilder
+  getQuery(): ISearchQuery
+}
+
+class QueryFactory {
+  public static init(log: Function): IQueryBuilder {
+    const query: ISearchQuery = {}
+    return {
+      addRuntime(runtime: IRuntime) {
+        log(`${chalk.green('Search condition')}: Runtime === ${runtime}`)
+        query.Runtime = runtime
+        return this
+      },
+      addSearchQuery(name: string) {
+        log(`${chalk.green('Search condition')}: FunctionName contains ${name}`)
+        query.name = name
+        return this
+      },
+      getQuery() {
+        return query
+      }
+    }
+  }
+}
+
+const regions = [
+  "eu-north-1",
+  "ap-south-1",
+  "eu-west-3",
+  "eu-west-2",
+  "eu-west-1",
+  "ap-northeast-2",
+  "ap-northeast-1",
+  "sa-east-1",
+  "ca-central-1",
+  "ap-southeast-1",
+  "ap-southeast-2",
+  "eu-central-1",
+  "us-east-1",
+  "us-east-2",
+  "us-west-1",
+  "us-west-2"
+]
 
 class LambdaFunctionSearch extends Command {
   static description = 'Search Lambda functions'
@@ -42,7 +88,7 @@ class LambdaFunctionSearch extends Command {
     // [For AWS SDK] region
     region: flags.string({
       char: 'r',
-      description: 'region'
+      description: 'region, (If you set "all", list all regions)'
     }),
     // [For AWS SDK] profile
     profile: flags.string({
@@ -58,6 +104,7 @@ class LambdaFunctionSearch extends Command {
       description: 'Show all function data',
       default: false
     }),
+
   }
   async listAllFunctions(query: ISearchQuery = {}, functions: Lambda.FunctionList = []): Promise<Lambda.FunctionList> {
     if (!this.client) {
@@ -83,29 +130,45 @@ class LambdaFunctionSearch extends Command {
     return items
   }
 
+  async getAllRegionFuncs(
+    query: ISearchQuery,
+    props: Lambda.ClientConfiguration = {},
+  ) {
+    return await Promise.all(regions.map(region => {
+      props.region = region
+      return this.worker(query, props)
+    }))
+  }
+
   async run() {
     const {flags} = this.parse(LambdaFunctionSearch)
+    const queryBuilder = QueryFactory.init(this.log)
+    if (flags.runtime) queryBuilder.addRuntime(flags.runtime)
+    if (flags.search) queryBuilder.addSearchQuery(flags.search)
     const props: Lambda.ClientConfiguration = {}
     if (!flags.region) this.log(chalk.yellow('warning') + ': Missing region')
-    if (flags.region) props.region = flags.region
+    if (flags.region) {
+      if (flags.region === 'all') return this.getAllRegionFuncs(queryBuilder.getQuery(), props)
+      props.region = flags.region
+    }
+    return this.worker(queryBuilder.getQuery(), props)
+  }
+
+  async worker(
+    query: ISearchQuery,
+    props: Lambda.ClientConfiguration = {},
+  ) {
+    const {flags} = this.parse(LambdaFunctionSearch)
     this.client = new Lambda(props)
     if (flags.profile) {
       const credentials = new SharedIniFileCredentials({profile: flags.profile})
       this.client.config.credentials = credentials
     }
-    const query: ISearchQuery = {}
-    if (flags.runtime) {
-      this.log(`${chalk.green('Search condition')}: Runtime === ${flags.runtime}`)
-      query.Runtime = flags.runtime
-    }
-    if (flags.search) {
-      query.name = flags.search
-      this.log(`${chalk.green('Search condition')}: FunctionName contains ${flags.search}`)
-    }
     try {
+      this.amount = 0
       const result = await this.listAllFunctions(query)
-      this.log(`${chalk.green('Matched Functions')}: ${result.length} / ${this.amount}`)
-      this.log('====')
+      this.log(`=== ${chalk.green('Matched Functions')}: ${result.length} / ${this.amount} ===`)
+      if (props.region) this.log(`${chalk.green('Region')} : ${props.region}`)
       result.forEach(item => {
         this.log(item.FunctionName)
         if (flags.showAll) console.log(item)
